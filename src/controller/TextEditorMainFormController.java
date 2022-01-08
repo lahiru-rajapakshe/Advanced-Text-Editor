45c47fae66d10c1b58792d77bfd829ea14468a42
LahiruRJ <sanlahiru720@gmail.com>
2022-01-08 08:31:30 +0530

    Add button variable declared and set button methods only, still not action performmed

--- src/controller/TextEditorMainFormController.java
+++ src/controller/TextEditorMainFormController.java
@@ -1,4 +1,69 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ToggleButton;
+
 public class TextEditorMainFormController {
+    public MenuItem btnSave;
+    public JFXButton btnSave2;
+    public JFXTextArea txtArea;
+    public ToggleButton btnBold;
+    public ToggleButton btnItalic;
+    public ToggleButton btnUndeline;
+    public JFXButton btnNew;
+    public JFXButton btnCut;
+    public JFXButton btnCopy;
+    public MenuItem menuSave;
+    public JFXButton btnOpen;
+    public JFXButton btnUp;
+    public JFXTextField txtSearch;
+    public JFXButton btnDown;
+    public JFXButton btnFind;
+    public Label lblFindCount;
+    public Label lblWordCount;
+    public JFXButton btnSaveMini;
+
+    public void btnSave2_OnAction(ActionEvent event) {
+    }
+
+    public void btnBold_OnAction(ActionEvent event) {
+    }
+
+    public void btnItalic_OnAction(ActionEvent event) {
+    }
+
+    public void btnUndeline_OnAction(ActionEvent event) {
+    }
+
+    public void btnNew_OnAction(ActionEvent event) {
+    }
+
+    public void btnCut_OnAction(ActionEvent event) {
+    }
+
+    public void btnCopy_OnAction(ActionEvent event) {
+    }
+
+    public void btnSave_OnAction(ActionEvent event) {
+    }
+
+    public void btnOpen_OnAction(ActionEvent event) {
+    }
+
+    public void btnUp_OnAction(ActionEvent event) {
+    }
+
+    public void btnDown_OnAction(ActionEvent event) {
+    }
+
+    public void txtSearcth_OnAction(ActionEvent event) {
+    }
+
+    public void btnFind_OnAction(ActionEvent event) {
+    }
 }
