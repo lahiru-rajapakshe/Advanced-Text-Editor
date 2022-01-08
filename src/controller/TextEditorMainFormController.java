a7ea6d94e35b1485582b2edffa57f347fd6cc24d
LahiruRJ <sanlahiru720@gmail.com>
2022-01-08 08:53:37 +0530

    Implement Open Button

--- src/controller/TextEditorMainFormController.java
+++ src/controller/TextEditorMainFormController.java
@@ -4,9 +4,10 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Label;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.ToggleButton;
+import javafx.scene.control.*;
+import javafx.stage.FileChooser;
+
+import java.io.*;
 
 public class TextEditorMainFormController {
     public MenuItem btnSave;
@@ -27,6 +28,12 @@ public class TextEditorMainFormController {
     public Label lblFindCount;
     public Label lblWordCount;
     public JFXButton btnSaveMini;
+    private final File file = null;
+    private final String ls = System.getProperty("line.separator");
+
+    public void initialize(){
+txtArea.clear();
+    }
 
     public void btnSave2_OnAction(ActionEvent event) {
     }
@@ -50,9 +57,33 @@ public class TextEditorMainFormController {
     }
 
     public void btnSave_OnAction(ActionEvent event) {
+      
     }
 
     public void btnOpen_OnAction(ActionEvent event) {
+        FileChooser chooser=new FileChooser();
+        chooser.setTitle("open a text file");
+        chooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Text file","*.txt"), new FileChooser.ExtensionFilter("All files","*.*"));
+        File file= chooser.showOpenDialog(null);
+        if (file!=null)
+        {
+            readFromFile(file);
+        }
+    }
+
+    private void readFromFile(File file) {
+        if (file != null) {
+            String text = "", buff = "";
+            try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+                text = br.readLine();
+                while ((buff = br.readLine()) != null) {
+                    text += ls + buff;
+                }
+            } catch (IOException ex) {
+                new Alert(Alert.AlertType.ERROR, ex.toString(), ButtonType.OK).show();
+            }
+            txtArea.setText(text);
+        }
     }
 
     public void btnUp_OnAction(ActionEvent event) {
