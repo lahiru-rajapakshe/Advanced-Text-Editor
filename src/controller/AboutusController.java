2023f8628717676d6b58f872f803025abf8a9044
LahiruRJ <sanlahiru720@gmail.com>
2022-01-09 08:48:30 +0530

    Readme.md Updated

--- src/controller/AboutusController.java
+++ src/controller/AboutusController.java
@@ -6,9 +6,11 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class AboutusController {
 
@@ -17,10 +19,13 @@ public class AboutusController {
 
     private Scene scene;
     public void cedits_OnAction(ActionEvent event) throws IOException {
-        AnchorPane load = FXMLLoader.load(this.getClass().getResource("./view/Credits.fxml"));
-    rootPane.getChildren().setAll(load);
-     
 
+
+//        Scene scene = new Scene(load);
+//        stage = new Stage();
+//        stage.setScene(scene);
+//
+//        stage.show();
     }
 
     public void aboutus_OnAction(ActionEvent event) {
